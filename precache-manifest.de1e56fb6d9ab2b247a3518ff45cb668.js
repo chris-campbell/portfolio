e39f9de91867b6d7aae6ecf1c60aaa447e668d1c
EC2 Default User <ec2-user@ip-172-31-24-138.us-east-2.compute.internal>
2019-12-08 17:57:33 +0000

    Updates

---
+++ precache-manifest.de1e56fb6d9ab2b247a3518ff45cb668.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86343d663c0d4c807d2d84bed0d5e7f0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "01fb475c004eb5dabfc1",
+    "url": "/portfolio/static/css/main.74dee7c6.chunk.css"
+  },
+  {
+    "revision": "e7d277b7c1e81326b4ea",
+    "url": "/portfolio/static/js/2.36a82b94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.36a82b94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01fb475c004eb5dabfc1",
+    "url": "/portfolio/static/js/main.d20d71ce.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "6f7e73222a19b4dcbe26815fc1fe3d46",
+    "url": "/portfolio/static/media/project-1.6f7e7322.png"
+  },
+  {
+    "revision": "4aedf992a363d44e60fbb40866046866",
+    "url": "/portfolio/static/media/project-2.4aedf992.png"
+  },
+  {
+    "revision": "8338c904a38ec27fcfec5de69575ef32",
+    "url": "/portfolio/static/media/project-3.8338c904.jpg"
+  }
+]);
